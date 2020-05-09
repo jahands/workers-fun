d446f1193f7b2201300cde98b41a2455b8942b26
Jacob Hands <jacob@gogit.io>
2020-05-09 16:22:16 -0500

    Convert to typescript

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,23 @@
+import { Router } from './router'
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const r = new Router()
+  // Replace with the approriate paths and handlers
+  r.get('.*/bar', () => new Response('responding for /bar'))
+  r.get('.*/foo', request => handler(request))
+  r.post('.*/foo.*', request => handler(request))
+  r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
+
+  r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
+
+  const resp = await r.route(request)
+  return resp
+}
+
+function handler(request) {
+  const init = {
+    headers: { 'content-type': 'application/json' },
+  }
+  const body = JSON.stringify({ some: 'json' })
+  return new Response(body, init)
 }
